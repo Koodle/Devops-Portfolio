a3e78b6fe51c8330ccdfa93b86f564534e56072f
Koodle <56200171+Koodle@users.noreply.github.com>
2025-08-12 10:16:59 +0100

    added tests todo

---
+++ __tests__/lib/actions.test.js
@@ -0,0 +1,6 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+
+describe('MyComponent', () => {
+    it.todo('should render correctly'); // This will not run and will be marked as a todo
+});
